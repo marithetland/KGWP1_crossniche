df6b39263c87b7ffd83105231336a614f3933ee7
Marit Hetland <marit.hetland@outlook.com>
2025-04-01 07:15:21 +0200

    fix hmrg command
    Added genome col to output

--- genotyping/get_HMRGs_from_bakta.sh
+++ genotyping/get_HMRGs_from_bakta.sh
@@ -2,18 +2,20 @@
 #2023-08-22
 #Get heavy metal and thermoresistance genes from bakta annotated assemblies
 
-# Make header
-echo -e "Sequence_ID\tType\tStart\tStop\tStrand\tLocus_Tag\tGene\tProduct\tDbXrefs"  > Bakta_v18_annotations.tsv
+#Make header
+echo -e "Genome\tSequence_ID\tType\tStart\tStop\tStrand\tLocus_Tag\tGene\tProduct\tDbXrefs" > Bakta_v18_annotations.tsv
 
-#Append all non-header lines from .tsv files 
-grep -h -v -e "#Sequence Id" -e "# Annotated with Bakta" -e "# Software: v1.8.1" -e "# Database: v5.0, full" -e "# DOI: 10.1099/mgen.0.000685" -e "# URL: github.com/oschwengers/bakta" hybrid_bakta_tsvs/*.tsv >> Bakta_v18_annotations.tsv
-grep -h -v -e "#Sequence Id" -e "# Annotated with Bakta" -e "# Software: v1.8.1" -e "# Database: v5.0, full" -e "# DOI: 10.1099/mgen.0.000685" -e "# URL: github.com/oschwengers/bakta" spades_bakta_tsvs/*.tsv >> Bakta_v18_annotations.tsv
+#Remove headers, add column for genome name
+for file in hybrid_bakta_tsvs/*.tsv; do
+    genome=$(basename "$file" .tsv)
+    grep -v -e "^#" "$file" | awk -v g="$genome" -F '\t' 'BEGIN {OFS="\t"} {print g, $0}' >> Bakta_v18_annotations.tsv
+done
 
-# Filter out HRMGs only
-# "Gene" is the 7th column
-cat Bakta_v18_3255_Illumina__2023-08-22.txt  | head -6 >> hmrgs_3255_best_assemblies.txt ; #What does this do?
-
-# "Gene" is the 7th column
-awk -F ‘\t’ ‘$7 ~ /nik|cpx|cus|cut|cue|cop|chr|zra|cor|rcn|cbt|cbi|dpp|opp|mnt|sit|mts|znu|zup|zit|zur|teh|ydc|yeb|tsg|yff|gol|ars|fie|yii|mer|ncr|pco|sil|ter|cad|czc|dsb|znt|cds/’ Bakta_v18_3255_best_assemblies__2023-08-22.txt >> hmrgs_3255_best_assemblies.txt ;
+#Filter out HRMGs only
+head -1 Bakta_v18_annotations.tsv > hmrgs_3255_best_assemblies.txt  
+# "Gene" is the 8th column
+awk -F '\t' '$8 ~ /nik|cpx|cus|cut|cue|cop|chr|zra|cor|rcn|cbt|cbi|dpp|opp|mnt|sit|mts|znu|zup|zit|zur|teh|ydc|yeb|tsg|yff|gol|ars|fie|yii|mer|ncr|pco|sil|ter|cad|czc|dsb|znt|cds/' Bakta_v18_annotations.tsv >> hmrgs_3255_best_assemblies.txt ;
 
 #Then parse in R with: parse_HMRGs.R
+
+
